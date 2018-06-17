1048659be4abf12a22dfb3b7428fdc4eb3ba4f8f
Amir HTH <uo240753@uniovi.es>
2018-06-17 18:24:34 +0200

    canvas broadcast completed with minor errors (drawer get duplicate shapes) drawed and broadcasted.

--- html/script.js
+++ html/script.js
@@ -6,20 +6,146 @@ if (window.WebSocket) {
 }
 
 window.addEventListener("load",init);
+window.addEventListener("load",initDraw);
 
-wsUri = "ws://echo.websocket.org";
+//wsUri = "ws://echo.websocket.org";
+wsUri = "ws://localhost:9001";
 
+
+function initDraw() {
+    console.log("initDraw entry with uri: "+wsUri);
+    initServer();
+    console.log("initDraw server inited");
+
+    canvas = new fabric.Canvas('canvas');
+    canvas.freeDrawingBrush.color = 'green';
+    canvas.freeDrawingBrush.lineWidth = 10;
+    //canvas.addEventListener("onmouseup", sendall);
+
+    addCircle.addEventListener('click', addCircleHandler);
+    addRectangle.addEventListener('click', addRectangleHandler);
+    addTriangle.addEventListener('click', addTriangleHandler);
+    pencil.addEventListener('click', pencilHandler);
+    selection.addEventListener('click', selectionHandler);
+
+    console.log("initDraw end");
+}
 function init() {
-    console.log("init entry");
-	output= document.getElementById("input");
+    console.log("init entry with uri: "+wsUri);
+	//output= document.getElementById("input");
     console.log("init output");
-	testWebSocket();
+	//testWebSocket();
     console.log("init end");
 }
 
+function isJson (str) {
+	try {
+		JSON.parse(str);
+	} catch (e) {
+		return false;
+    }
+    return true;
+}
+
+function addCircleHandler() {
+	var obj = {
+		radius : 20,
+		top    : 100,
+		left   : 100,
+		fill   : 'green'
+	};
+	sendObject('Circle', obj);
+}
+
+function addRectangleHandler() {
+    var obj = {
+        top    : 200,
+        left   : 150,
+        width  : 60,
+        height : 90,
+        fill   : 'red'
+    };
+    sendObject('Rectangle', obj);
+}
+
+function addTriangleHandler() {
+    var obj = {
+        top    : 50,
+        left   : 50,
+        width  : 20,
+        height : 30,
+        fill   : 'blue'
+    };
+    sendObject('Triangle', obj);
+}
+
+function pencilHandler() {
+	canvas.isDrawingMode=true;
+}
+
+function selectionHandler() {
+	canvas.isDrawingMode =false;
+}
+
+
+function initServer() {
+	// unificar con el testWebSocket
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = connectionOpen;
+    websocket.onmessage = onMessageFromServer;
+}
+
+function connectionOpen() {
+	websocket.send('connection open');
+}
+
+function onMessageFromServer(message) {
+	console.log('received: '+ message);
+    console.log('received data: '+ message.data);
+	if (isJson(message.data)) {
+		var obj = JSON.parse(message.data);
+		console.log("got data from server");
+		drawRecivedObj(obj.type, obj.data);
+	} else {
+		// si no es un json será un mensaje del chat ?
+	}
+}
+
+function addObject(type, obj) {
+    websocket.send(JSON.stringify({
+        'type': type,
+        'data': obj
+    }))	;
+}
+
+function sendObject(type, obj) {
+    drawRecivedObj(type,obj);
+	addObject(type,obj);
+
+}
+
+function drawRecivedObj(type, obj) {
+    var shape;
+    if (type == 'Triangle') {
+
+        shape= new fabric.Triangle(obj)
+
+    } else if (type == 'Rectangle') {
+
+        shape= new fabric.Rect(obj)
+
+    } else if (type == 'Circle') {
+
+        shape= new fabric.Circle(obj)
+
+    }
+    canvas.add(shape);
+}
+
+
 function testWebSocket() {
     console.log("testWebSocket entry");
-	websocket = new WebSocket(wsUri);
+	websocket = new WebSocketty(wsUri);
     console.log("testWebSocket metods")
 	websocket.onopen = onOpen; 
 	websocket.onclose = onClose; 
---
+++ server.js
@@ -0,0 +1,18 @@
+var WebSocketServer = require('ws').Server;
+wss = new WebSocketServer ({ port:9001 });
+var clients = [];
+
+
+wss.on('connection', function connection(ws) {
+    //var con = request.accept('any-protocol', request.origin)
+    clients.push(ws)
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        clients.forEach(function(client) {
+
+            client.send(message);
+
+        });
+    });
+});
+
