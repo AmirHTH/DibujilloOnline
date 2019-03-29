88a0dcac32dbdc9d4a4ff7e158c8c030ab07efe0
Amir HTH <uo240753@uniovi.es>
2019-03-29 18:26:21 +0100

    galeria hecha pero en feo

---
+++ html/blog/scriptCocina.js
@@ -0,0 +1,13 @@
+function on(imgen) {
+
+    document.getElementById("picplace").src=imgen.src;
+    document.getElementById("picasa").style.display = "block";
+
+
+}
+
+function off() {
+
+    document.getElementById("picasa").style.display = "none";
+
+}
