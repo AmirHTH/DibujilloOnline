5632fe77a1fc2656dd1b7fd36eb46d3e7405b93a
Amir HTH <uo240753@uniovi.es>
2019-04-01 01:39:06 +0200

    ip

--- html/script.js
+++ html/script.js
@@ -8,7 +8,7 @@ if (window.WebSocket) {
 //window.addEventListener("load",init);
 
 //wsUri = "ws://echo.websocket.org";
-wsUri = "ws://localhost:9001";
+wsUri = "ws://88.9.159.192:9001";
 
 var ID = Math.floor((Math.random() * 100000000) + 2);
 
